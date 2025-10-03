8bf89f75cf2dbe5bab3289e7b2fcdfd26a41bdf5
AndresRPY <rodrigorealpe@unicomfacauca.edu.co>
2025-10-02 22:35:07 -0500

    subida inicial

---
+++ main.tf
@@ -0,0 +1,28 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${var.prefix}-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "${var.prefix}-dns"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+
+  default_node_pool {
+    name       = "nodepool"
+    node_count = var.node_count
+    vm_size    = "Standard_B2s"
+  }
+
+
+  network_profile {
+    network_plugin = "azure"
+    load_balancer_sku = "standard"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  type    = string
+  default = "parcial"
+}
+
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "node_count" {
+  type    = number
+  default = 2
+}
